994aca6286b7a0fa06b409cacbd37dd2251fbe37
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2021-11-06 17:15:11 +0100

    google drive imp update

--- src/main/java/GoogleDriveUser.java
+++ src/main/java/GoogleDriveUser.java
@@ -1,12 +1,18 @@
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import storageSpec.*;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
 import java.util.*;
+
 
 public class GoogleDriveUser extends AbstractUser {
     /**
@@ -58,7 +64,7 @@ public class GoogleDriveUser extends AbstractUser {
         }
         System.out.println("Folder ID: " + file.getId() + "; " + "folder name: "+ file.getName());
 
-        Storage storage = new Storage(storageName, this, rootLocation);
+        Storage storage = new Storage(storageName, this, rootLocation, file.getId());
         super.addStorage(storage, Privilege.ADMIN);
 
     }
@@ -69,18 +75,66 @@ public class GoogleDriveUser extends AbstractUser {
     }
 
     @Override
-    public void createDir(String s, String s1) {
+    public void createDir(String dir, String path) {
+        File fileMetadata = new File();
+        fileMetadata.setName(dir);
+        String folderId = this.findStorageID(path);
+        fileMetadata.setParents(Collections.singletonList(folderId));
+        fileMetadata.setMimeType("application/vnd.google-apps.folder");
 
+        File file = null;
+        try {
+            file = driveService.files().create(fileMetadata)
+                    .setFields("id")
+                    .execute();
+            file.setName(dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Folder ID: " + file.getId() + "; " + "folder name: "+ file.getName());
     }
 
     @Override
     public void createDir(String s, String s1, String s2, int i) {
 
     }
-
+    /**
+     * Uploads existing file with given name, with given type(txt, png...), from path(location on local file system) to google drive storage with given storageName
+     */
     @Override
-    public void createFile(String s, String s1) {
-
+    public void uploadExistingFile(String fileName, String path, String storageName, String fileType) {
+        String folderId = this.findStorageID(storageName);
+        File fileMetadata = new File();
+        fileMetadata.setName(fileName);
+        fileMetadata.setParents(Collections.singletonList(folderId));
+        java.io.File filePath = new java.io.File(path);
+        FileContent mediaContent = new FileContent(fileType, filePath);
+        File file = null;
+        try {
+            file = driveService.files().create(fileMetadata, mediaContent)
+                    .setFields("id, parents")
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("File ID: " + file.getId());
+    }
+    /**
+     * Creates file on specified path on local file system and then uploads it on google drive storage with given storageName
+     */
+    @Override
+    public void createFile(String fileName, String path, String storageName, String fileType) {
+        Path filepath = Paths.get(path); //creates Path instance
+        try
+        {
+            Path p= Files.createFile(filepath);     //creates empty file at specified location(path) on local file system
+            System.out.println("File Created at Path: "+p);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        this.uploadExistingFile(fileName, path, storageName, fileType);
     }
 
     @Override
@@ -149,13 +203,21 @@ public class GoogleDriveUser extends AbstractUser {
     }
 
     @Override
-    public void setStorageSize(int i, Storage storage) {
-
+    public void setStorageSize(int size, Storage storage) {
+        if(super.getStoragesAndPrivileges().get(storage) != Privilege.ADMIN){
+            System.out.println("Samo admin moze da definise velicinu skladista u bajtovima");
+            return;
+        }
+        storage.setStorageSize(size);
     }
 
     @Override
-    public void setForbiddenExtensions(Collection<String> collection, Storage storage) {
-
+    public void setForbiddenExtensions(Collection<String> ext, Storage storage) {
+        if(super.getStoragesAndPrivileges().get(storage) != Privilege.ADMIN){
+            System.out.println("Samo admin moze da definise koje su zabranjene ekstenzije u skladistu");
+            return;
+        }
+        storage.setForbiddenExtensions(ext);
     }
 
     @Override
@@ -165,13 +227,36 @@ public class GoogleDriveUser extends AbstractUser {
 
     @Override
     public void addUser(AbstractUser abstractUser, Storage storage, Privilege privilege) {
+        if(super.getStoragesAndPrivileges().get(storage) != Privilege.ADMIN){
+            System.out.println("Samo admin moze da doda korisnika");
+            return;
+        }
         storage.addUser(abstractUser);//sluzi da storage zna da ovaj user moze da mu pristupi
         abstractUser.addStorage(storage, privilege);//sluzi da user zna da za ovaj storage ima ovaj nivo privilegije
     }
 
     @Override
     public void removeUser(AbstractUser abstractUser, Storage storage) {
+        if(super.getStoragesAndPrivileges().get(storage) != Privilege.ADMIN){
+            System.out.println("Samo admin moze da obrise korisnika");
+            return;
+        }
         storage.removeUser(abstractUser);
         abstractUser.removeStorage(storage);
+    }
+    private String findStorageID(String storageName){
+        for (Storage storage: super.getStoragesAndPrivileges().keySet()) {
+            if(storage.getStorageName().equalsIgnoreCase(storageName)){
+                return  storage.getStorageID();
+            }
+        }
+        System.out.println("ERROR: ID for storage with name: " + storageName + " has not been found");
+        return "error";
+    }
+    //here i stopped; i need this method to create file on specified path on google drive
+    private String findParentDirID(String storagePath){
+        String[] array = storagePath.split("/");
+
+       return null;
     }
 }
--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,3 +1,8 @@
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import storageSpec.AbstractUser;
 
 import java.io.IOException;
@@ -5,9 +10,15 @@ import java.security.GeneralSecurityException;
 
 public class TestClass {//delete this class later
     public static void main(String... args) throws IOException, GeneralSecurityException {
-        //AbstractUser user = new GoogleDriveUser();
-        //user.initStorage("myStorage", "drive");
+//        AbstractUser user = new GoogleDriveUser();
+//        user.setUserName("ntadic");
+//        user.setPassword("mypass");
+//        user.initStorage("novoskladisteee", "drive");
+//        user.uploadExistingFile("mojNoviFajl", "C:\\Users\\tadic\\Desktop\\aaa.txt", "novoskladisteee", "/txt");
+//        user.createFile("fileName", "C:\\Users\\tadic\\Desktop\\probaaaamooo.txt", "novoskladisteee", "/txt");
+//        user.createDir("nestedDir", "novoskladisteee");
         /*
+        Primer za listanje fajlova
         // Print the names and IDs for up to 10 files.
         FileList result = service.files().list()
                 .setPageSize(10)
@@ -23,5 +34,25 @@ public class TestClass {//delete this class later
             }
         }
          */
+        //"novoskladisteee/nestedDir"
+        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        String pageToken = null;
+        Drive driveService;
+        driveService = new Drive.Builder(HTTP_TRANSPORT, DriveQuickstart.getJsonFactory(), DriveQuickstart.getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(DriveQuickstart.getApplicationName())
+                .build();
+        do {
+            FileList result = driveService.files().list()
+                    .setSpaces("drive")
+                    .setFields("nextPageToken, files(id, name)")
+                    .setPageToken(pageToken)
+                    .execute();
+            for (File file : result.getFiles()) {
+                if(file.getName().equalsIgnoreCase("novoskladisteee")){
+                    //file.get
+                }
+            }
+            pageToken = result.getNextPageToken();
+        } while (pageToken != null);
     }
 }
